d4331579ebbf9154dd2d1242d788a8c50f446b44
ZWRKje <andrey09767@gmail.com>
2021-02-24 22:35:14 +0300

    добавлен подсчёт кадратуры, амплитуды и фазы

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	ofstream out("..\\output1.txt");
+    ofstream sinFile,cosFile,amplFile,phaseFile;
+    sinFile.open("sin.txt");//синусойда
+    cosFile.open("cos.txt");//косинусойда
+    amplFile.open("ampl.txt");//амплитуда квадратур
+    phaseFile.open("phase.txt");//фаза
 
 	//Исходные данные:
 	int min = -300;
@@ -19,24 +23,35 @@ int main()
 	int distance = 5000;
 	double waveLen = 0.03;
 
-	double Rt = 0;
+	double rt = 0;
 	int step = 0;//шаг измерения 
-	double value = 0.0;
+	double valueSin,valueCos,valuePhase,valueAmpl = 0.0;
 
 	cout << "Введите шаг для измерения: ";
 	cin >> step;
-
+ 
 	for (int i = min; i <= max; i += step)// цикл по точкам на земле
 	{
 		for (double j = -5.0; j <= 5.0; j += 2.5)//цикл по точкам на антенне
 		{
-			Rt = (sqrt(pow(i - j, 2) + pow(-distance, 2)));
-			value += sin(Rt / waveLen);
-		}
-		out << "x = " << setw(4) << i << "\tS = " << setw(2) << value << "\n";
-		value = 0;
+			rt = (sqrt(pow(i - j, 2) + pow(-distance, 2)));
+			valueSin += sin(rt / waveLen);
+            valueCos += cos(rt / waveLen);
+            valueAmpl = sqrt( pow(valueCos,2) + pow(valueSin,2));
+            valuePhase = atan(valueSin/valueCos);
+        }
+        sinFile << "\n  " << setw(4) << i << "\t " << setw(2) << valueSin;
+	cosFile << "\n  " << setw(4) << i << "\t " << setw(2) << valueCos;
+        phaseFile << "\n  " << setw(4) << i << "\t " << setw(2) << valuePhase;
+        amplFile << "\n  " << setw(4) << i << "\t " << setw(2) << valueAmpl;
+        valuePhase = valueAmpl = valueCos = valueSin = 0;
 	}
-
-	cout << "\n" << "Результаты измерений записанны в файл output1.txt" << " \n ";
+  
+    sinFile.close();
+    cosFile.close();
+    phaseFile.close();
+    amplFile.close();
+    cout << "\n" << "Результаты измерений записанны в файлы sin.txt cos.txt phaze.txt ampl.txt" << " \n ";
+	
 	return 0;
 }
